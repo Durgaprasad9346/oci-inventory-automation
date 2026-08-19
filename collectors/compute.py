f8e334cd05b3d5a4a0e7d662f48b5258ca8e6d74
Durgaprasad9346 <142432429+Durgaprasad9346@users.noreply.github.com>
2026-08-19 12:13:45 +0530

    Update compute.py

--- collectors/compute.py
+++ collectors/compute.py
@@ -2,40 +2,52 @@ import oci
 
 from collectors.base import Resource
 from utils.compartments import get_compartments
+from utils.regions import get_regions
 
 
 def collect_compute(config):
     """
     Collect all OCI Compute instances
-    from all accessible active compartments.
+    from all accessible compartments
+    across all subscribed regions.
     """
 
-    compute_client = oci.core.ComputeClient(config)
-
     compartments = get_compartments(config)
+    regions = get_regions(config)
 
     resources = []
 
-    for compartment in compartments:
+    for region in regions:
 
-        instances = oci.pagination.list_call_get_all_results(
-            compute_client.list_instances,
-            compartment["id"],
+        print(f"  Processing Compute region: {region}")
+
+        region_config = config.copy()
+        region_config["region"] = region
+
+        compute_client = oci.core.ComputeClient(
+            region_config
         )
 
-        for instance in instances.data:
+        for compartment in compartments:
 
-            resources.append(
-                Resource(
-                    service="Compute",
-                    resource_type="Instance",
-                    name=instance.display_name,
-                    ocid=instance.id,
-                    compartment_id=compartment["id"],
-                    compartment_name=compartment["name"],
-                    region=config["region"],
-                    state=instance.lifecycle_state,
-                )
+            instances = oci.pagination.list_call_get_all_results(
+                compute_client.list_instances,
+                compartment["id"],
             )
+
+            for instance in instances.data:
+
+                resources.append(
+                    Resource(
+                        service="Compute",
+                        resource_type="Instance",
+                        name=instance.display_name,
+                        ocid=instance.id,
+                        compartment_id=compartment["id"],
+                        compartment_name=compartment["name"],
+                        region=region,
+                        state=instance.lifecycle_state,
+                    )
+                )
 
     return resources
